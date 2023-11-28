09f6162a5dd9861d0a1b3f7a0cd5821f328eedb7
karimkhallaf <karimnkhallaf@gmail.com>
2023-11-28 10:49:42 +0400

    broken_pirate fixed

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?")#added missing qoutation marks
+if greeting in ["Arrr!"]:#the bracket was changed into a sqaured bracket 
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: # I changed elif command into else and I added colon
+        print("Greetings, hater of pirates!") # I added an indentation   
